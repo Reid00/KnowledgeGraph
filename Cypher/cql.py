69fdc97971165d798df1bcdb21925697ceb2f25a
Reid <isreid.zhang@outlook.com>
2020-03-07 22:27:02 +0800

    add delete duplicated nodes

--- Cypher/cql.py
+++ Cypher/cql.py
@@ -27,6 +27,8 @@ class CypherExercise():
         res=self.graph.run(query).data
         print(res)
 
+        #删除重复节点，可用id 不同其他相同
+
         #带有关系的查询
         q='match (p:Person)-[h:Has_Phone]->(p1:Phone) return p,p1 limit 10'
         # 带有where 条件的查询
@@ -46,7 +48,8 @@ class CypherExercise():
 
         # 创建关系
         q='Create (p:Person)-[l:Love]->(d:Dog)'
-
+        # 创建多维复杂关系
+        q='match (p:Person{name:"李四"}),(p1:Person{name:"赵四"}),(p2:Person{name:"王五"}) CREATE (p)-[h:Hate]->(p1)<-[l:Love]-(p2) return p,p1,p2,h,l'
         # 创建带有属性的实体，关系
         q='Create (p:Person{name:"力气"})-[:Fear{Level:1}]->(t:Tiger{type: "东北话"})'
 
@@ -54,16 +57,34 @@ class CypherExercise():
         q='match (p:Person{name:"力气"}),(p1:Person{name:"张三"}) Create(p)-[k:Know]->(p1)'
 
         #merge 有则返回，没有则创建
-        q='match (p:Person{name:"力气"}),(p1:Person{name:"张三"}) Merge(p)-[k:Know]->(p1)'
+        q='Merge (p:Person{name:"力气"}),(p1:Person{name:"张三"}) Merge(p)-[k:Know]->(p1)'
 
-        # 删除实体关系
-        q='match (p:Person{name:"力气"})-[f:Fear]->(t:Tiger) delete f'
+
+        # 删除所有的节点和关系
+        q='match (p:Person) delete p'
+        # 同时删除关系；这个查询适用于删除少量的数据，不适用于删除巨量的数据
+        q='match (p:Person) detach delete p'
+        # 删除一个节点及其所有的关系
+        q='match (p:Person{name:"力气"}) detach delete p'
+        # 只删除实体
         q='match (p:Person{name:"力气"})-[f:Fear]->(t:Tiger) delete p'
         #同时删除
         q='match (p:Person)-[l:Love]->(d:Dog) delete p,l,d'
+        # 删除实体关系
+        q='match (p:Person{name:"力气"})-[f:Fear]->(t:Tiger) delete f'
 
-        #更新实体
+        # 删除一个属性;neo4j不允许属性存储空值null。如果属性的值不存在，那么节点或者关系中的属性将被删除。这也可以通过remove来删除。
+        q='match (p:Person{name :"Andres"}) remove p.age return p'
+        # 删除节点的一个标签
+        q='match (p:Person{name: "张三"}) remove p:Chinese return p'
+        # 删除节点的多个标签
+        q='mathc (p:Person{name:"张三"}) remove p:Chinise:Student return p'
+
+
+        #更新实体，添加标签animal
         q='match (t:Tiger) where id(t)==1837 set t:Animal return Animal'
+        # 给实体添加更多标签
+        q='match (p:Person{name:"李四"}) set p:Chinese:Student return p'
 
         #给实体增加属性
         q='match (a:Animal) where id(a)==1837 set a.age==10'
@@ -86,17 +107,87 @@ class CypherExercise():
         # 查询姓名2 和 姓名10 的所有的最短路径
         q='match (p1:Person{name:"姓名2"}), (p2:Person{name:"姓名10"}) p=allshortestpaths((p1)-[*..10]-(p2)) return p'
 
+        # with从句可以连接多个查询的结果，即将上一个查询的结果用作下一个查询的开始。with的每一个结果，必须使用别名标识
+        #两种用法：
+        #1、通过使用oder by 和limit，with可以限制传入下一个match子查询语句的实体数目。
+        #2、对聚合值过滤。
+        # 3. collect 返回一个list
+        # sample: 
+        q='MATCH (n) WITH n ORDER BY n.name DESC LIMIT 3 RETURN collect(n.name)'
 
-        # 将hudong_pedia.csv 导入
-        load='LOAD CSV WITH HEADERS  FROM "file:///hudong_pedia.csv" AS line CREATE (p:HudongItem{title:line.title,image:line.image,detail:line.detail,url:line.url,openTypeList:line.openTypeList,baseInfoKeyList:line.baseInfoKeyList,baseInfoValueList:line.baseInfoValueList})'
+        # 利用with n.name 把传给了后面, 用collect 对名字重复的过滤
+        q='MATCH (p:Person) with p.name AS Name, collect(p) AS nodes where size(nodes)>1 return nodes'
+        q='MATCH (p:Person) with p.name AS Name, collect(p) AS nodes where size(nodes)>1 FOREACH(i in tail(nodes)|DETACH DELETE i) return i'
+    
+    def load_data_csv(self):
+        #load node csv 
+        command='USING PERIODIC COMMIT 1000 LOAD CSV WITH HEADERS FROM "file:///nodes.csv" AS csvLine CREATE (c:Contact { mobile:csvLine.mobile, name:csvLine.name, updateTime:csvLine.updateTime, createTime:csvLine.createTime })'
+        # USING PERIODIC COMMIT 1000,是满足1000条之后，提交一个事务，这样能够提高效率。
+        # 导入节点之后，我们必然会导入关系。这里就有个坑，如果你在node节点的库里，没有创建index，那么导入关系的时候，将会慢的要死。
+        # 创建索引之前，我们插入的节点数据有可能会有重复的情况，我们需要先清除一下重复数据。
+        command='MATCH (n:Contact) WITH　n.mobile AS mobile , collect (n) AS nodes WHERE size(nodes)>1 FOREACH (n in tail(nodes)|DETACH DELETE n)'
 
-        # 创建索引
-        create_index='CREATE CONSTRAINT ON (c:HudongItem) ASSERT c.title IS UNIQUE'
-        
+        #创建索引
+        index1= 'CREATE CONSTRAINT ON (c:Contact) ASSERT c.mobile IS UNIQUE'
+        index2= 'CREATE INDEX ON :Contact(mobile)'
         # 删除索引
         drop_index='DROP CONSTRAINT ON (c:HudongItem) ASSERT c.title IS UNIQUE'
+        #导入关系csv文件
+        rel='USING PERIODIC COMMIT 1000 LOAD CSV WITH HEADERS FROM "file:/rels.csv" AS csvLine MATCH (c:Contact {mobile:csvLine.mobile1}),(c1:Contact {mobile:csvLine.mobile2}) CREATE (c)-[:hasContact]->(c1)'
+        
+         # 将hudong_pedia.csv 导入
+        load='LOAD CSV WITH HEADERS  FROM "file:///hudong_pedia.csv" AS line CREATE (p:HudongItem{title:line.title,image:line.image,detail:line.detail,url:line.url,openTypeList:line.openTypeList,baseInfoKeyList:line.baseInfoKeyList,baseInfoValueList:line.baseInfoValueList})'
+        # 创建索引
+        create_index='CREATE CONSTRAINT ON (c:HudongItem) ASSERT c.title IS UNIQUE'     
+        # 导入hudongItem和新加入节点之间的关系
+        q='LOAD CSV  WITH HEADERS FROM "file:///wikidata_relation2.csv" AS line MATCH (entity1:HudongItem{title:line.HudongItem}),(entity2:NewNode{title:line.NewNode}) CREATE (entity1)-[:RELATION {type: line.relation}]->(entity2)'
+        
+    def py_cql(self,Person,name,age,title,update_time):
+        # 创建name唯一性，name是profile_id
+        command = f'CREATE CONSTRAINT ON (n:{Person}) ASSERT n.name IS UNIQUE'
+        self.graph.run(command)
+
+        # 删除一个项目的全部节点和边
+        command= f'MATCH (n:{Person}) DETACH DELETE n'
+        # 建节点，原节点没有就会建，属性和属性的值完全一样才会保留原数据
+        command = f'MERGE (node:{Person}{name:{name},age:{age},title:{title}}) return node'
+        # 删除节点和关联的边
+        command= f'MATCH (node:{Person}) WHERE node.update_time< {update_time} DETACH DELETE node'
+        return None
+
+    def create_rules(self):
+        # 一度关系内有多少个触碰黑名单的
+        q='match (p:Person)-[]-(p1:Person)-[h:Has_Phone]-(b:Black) where p.name=="张三" return count(b)'
+        # 申请人的二度关系中有多少触碰黑名单
+        q='match (p:Person)-[]-(p1:Person)-[]-(p2:Person)-[h:Has_Phone]-(b:Black) where p.name=="张三" return count(b)'
+
+    def find_subgraph_by_entity(self,name):
+        #根据实体名称进行查询，考虑方向,返回关系
+        command=f'MATCH (p:Person{name:{name}})-[rel]->(p2:Person) return p,k,p2'
+        ans=self.graph.run(command).data()
+        if (len(ans)==0):
+            #查询实体，不考虑其方向
+            command=f'MTACH (p:Person)-[rel]-(p2:Person) where p.name={name} return p,rel,p2'
+            ans=self.graph.run(command).data()
+        return ans
+
+    def find_subgraph_by_entity2(self,name):
+        command=f'MATCH　(p:Person)-[rel]-(p1:Person{name:{name}}) return p,rel,p1'
+        ans=self.run(command).data()
+        if (len(ans)==0):
+            command=f'MATCH (p:Student)-[rel]-(p1:Person{name:{name}} return p,rel,p1)'
+            ans=self.run(command).data()
+        return ans
+    
+    def find_subgraph_by_rel_entity(self,rel,name):
+        command=f'MATCH (p:Person{name:{name}})-[rel:{rel}]-(p1:Person) return p,rel,p1'
+        ans=self.run(command).data()
+        return ans
+
+    
 
 
+    
 if __name__ == "__main__":
     cypher= CypherExercise()
     cypher.connectionDB()
--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # KnowledgeGraph
 KowledgeGraph test
+
+
+
+### 1. 使用load csv导入数据
+
+1. 导入CSV 节点 （detail see the code）
+2. 创建索引并删除重复节点
+3. 导入关系csv文件
+
+### 2. 使用neo4j-admin导入数据
+
+通过neo4j-admin方式导入的话，需要暂停服务，并且需要清除graph.db,这样才能导入进去数据。而且，只能在初始化数据时，导入一次之后，就不能再次导入了。所以这种方式，可以在初次建库的时候，导入大批量数据，等以后如果还需要导入数据时，可以采用上边的方法。
+
+1. #### 数据导入前的准备工作
+
+   对于大规模的数据集，使用语句插入和load csv的时候往往非常缓慢，当需要插入大量三元组时，可以考虑使用Neo4j-import的方式。这种方式有许多**注意点**：
+
+   1、传入文件名的时候务必使用绝对路径。
+   2、在执行指令之前务必保证Neo4j处于关闭状态，如果不确定可以在Neo4j根目录下运行./bin/neo4j status 查看当前状态。
+   3、使用neo4j-admin import指令导入之前先将原数据库从neo4j_home/data/databases/graph.db／中移除。
+   4、写CSV文件的时候务必确保所有的节点的CSV文件的ID fileds的值都唯一、不重复。并且确保所有的边的CSV文件的START_ID 和 END_ID都包含在节点CSV文件中。
+
+2. #### 数据预处理
+
+   neo4j-import官方要求的数据格式为csv文件，主要就是分成两个文件entity.csv 和relationship.csv。
+
+   其中entity.csv中包含了实体的id，实体的name，以及标签LABEL，具体格式如下：
+
+   ```
+   entity:ID,name,:LABEL
+   e0,GDP,my_entity
+   e1,PHP,my_entity
+   e2,李冲,my_entity
+   e3,perimenopausal syndrome,my_entity
+   e4,雁荡山景区分散，东起羊角洞，西至锯板岭；南起筋竹溪，北至六坪山。,my_entity
+   e5,词条（拼音：cí tiáo）也叫词目，是辞书学用语，指收列的词语及其释文。,my_entity
+   e6,芦苇茂密，结草为荡,my_entity
+   e7,面粉，水，酵母，苏打,my_entity
+   e8,先注册先得的原则,my_entity
+   e9,解压缩软件,my_entity
+   e10,华硕电脑股份有限公司,my_entity
+   ```
+
+   relationship.csv文件包含了起始节点id，结束节点id，关系的name，以及标签LABEL，具体格式如下：
+
+   ```
+   :START_ID,:END_ID,name,:TYPE
+   e48,e799,属性,属性
+   e191,e479,描述,描述
+   e641,e5,描述,描述
+   e641,e182,标签,标签
+   e237,e575,描述,描述
+   e237,e237,中文名,中文名
+   e237,e533,是否含防腐剂,是否含防腐剂
+   e237,e264,主要食用功效,主要食用功效
+   e237,e405,适宜人群,适宜人群
+   ```
+
+   需要将导入的数据转换成这样的两个格式的csv文件，才能够导入Neo4j中。
+
+3. #### 数据导入
+
+   csv数据文件准备好后，可以通过执行以下脚本来实现数据导入：
+
+   ```#导入命令
+   
+   #导入命令
+   # 停止neo4j服务
+   neo4j stop 
+   # 如果是Linux可以进入到databases目录下删除数据库，windows直接删除即可
+   cd /usr/local/Cellar/neo4j/3.5.0/libexec/data/databases
+   rm -rf graph.db
+   # 执行数据导入命令neo4j-admin
+   neo4j-admin import \
+   --database=graph.db
+   --nodes:phone="../phone_header.csv,phones.csv \
+   --ignore-duplicate-nodes=true \
+   --ignore-missing-nodes=true \
+   --relationships:call="../call_header.csv,call.csv"
+   # 重启neo4j服务
+   neo4j start
+   ```
